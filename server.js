e7c3fdda058a4fec702ccccddd5355a8212e77a0
ShabbirAhmedShovon <shabbirahmedshovon@gmail.com>
2025-03-30 13:31:05 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -6,12 +6,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/AccountManagement', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.error("MongoDB Connection Error:", err));
+mongoose.connect('mongodb://localhost:27017/AccountManagement')
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
